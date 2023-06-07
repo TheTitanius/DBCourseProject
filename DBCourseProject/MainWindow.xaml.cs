ada24c73bad8161fbf5fa2c8f14568f27795f3ee
Александр Мигас <migas.alexandr@gmail.com>
2023-06-07 22:59:52 +0300

    Сам проект

--- DBCourseProject/MainWindow.xaml.cs
+++ DBCourseProject/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using DBCourseProject.Entities;
+﻿using DBCourseProject.ViewModels;
 using System.Windows;
 
 namespace DBCourseProject
@@ -8,22 +8,15 @@ namespace DBCourseProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Context db;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            using (var context = new Context())
-            {
-                
-                Client client = new() { FullName = "Мигас Александр Сергеевич", Telephone = "+79516885750", BankAccount = 12345678901};
-
-                context.Clients.Add(client);
-                context.SaveChanges();
-                
-            }
+            db = new Context();
+            ClientItem.DataContext = new ClientViewModel(db);
+            ManufacturerItem.DataContext = new ManufacturerViewModel(db);
+            TVItem.DataContext = new TVViewModel(db);
+            ContractsItem.DataContext = new ContractViewModel(db);
         }
     }
 }
